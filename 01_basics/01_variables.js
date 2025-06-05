4bb73d03844f667f8333763c69f79dbaa1c56960
RehamanKhan <rehamankhan9517@gmail.com>
2025-06-06 00:12:31 +0530

    add my all js learning files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,37 @@
+// const accountid = 124;
+// let accoutemail = "rehamankhan8960@gmail.com"
+// var accoutpassword = "12345"
+// accountcity = "hardoi"  
+
+// // accountid = 3
+// accoutemail ="rehaman@gmail.com"
+// // accoutpassword = 122
+// accountcity = "uttarpradesh"
+
+// console.log(accountid);
+
+//  console.table([accoutemail, accoutpassword, accountcity])
+//  console.log(typeof accountid, typeof accountcity);
+  
+
+// primitive data......
+
+
+let a = "rk"
+let b = 22
+let c = 32.2    
+const d = true;
+let e = undefined;                    
+let f = null;
+ 
+console.log(a,b,c,d,e,f);
+console.log( typeof  a, typeof b, typeof c, typeof d, typeof e, typeof f);
+
+
+
+ let o = {
+    name : "rk",
+    "job role": 56444
+} 
+
+console.log(o);
---
+++ 01_basics/test.js
@@ -0,0 +1,1 @@
+console.log("rehaman khan")
