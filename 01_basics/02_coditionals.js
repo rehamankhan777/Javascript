ff20b9968da08a6d0196523d1858e7dc75797a6d
RehamanKhan <rehamankhan9517@gmail.com>
2025-06-07 17:04:30 +0530

    add my js practice file

---
+++ 01_basics/02_coditionals.js
@@ -0,0 +1,39 @@
+//  let age = 5;
+
+// if(age>18) {
+// console.log("you can drive");
+// }
+
+// else{
+//     console.log("you cannot drive");
+// }
+
+// let age = 10;
+// let grace =5;
+
+// age +=grace     
+// console.log(age) ;
+
+
+// let age = 18;
+
+// if(age==18){   
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+    
+// }
+
+// for  checking.....
+
+
+let age = 17;
+
+if(age!=18){
+    console.log("true");
+}
+else{
+    console.log("false");
+    
+}
