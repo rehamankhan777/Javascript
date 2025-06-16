60ecf82986b4d400897918b15a862bb1f6604360
RehamanKhan <rehamankhan9517@gmail.com>
2025-06-16 22:59:25 +0530

    add my loop practice file

---
+++ 01_basics/03_loops.js
@@ -0,0 +1,26 @@
+// for(let i=1; i<=5; i++){
+//     console.log("rehamankhan"); 
+// }
+
+// calculate sum of 1 to 5
+
+// let sum = 0;
+// for(let i=1; i<=5; i++){
+//     sum =sum+i;   
+//    console.log("sum = ",sum);
+    
+// }
+//   console.log("sum = ",sum);
+
+
+// let i=1;
+// while (i<=5){
+//   console.log("Rk");
+//   i++;
+// }
+
+// let i=20;
+// do{
+//   console.log("rehaman");
+//   i++;
+// } while(i<=10);
